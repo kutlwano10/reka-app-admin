313c87c93f024ef267c900a318d363bb557e00c1
kutlwano10 <kutlwano.ramotebele@gmail.com>
2024-09-18 16:35:41 +0200

    fixed mongoDB uri

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,9 +1,9 @@
 import clientPromise from "@/lib/mongodb";
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
-import NextAuth from "next-auth/next";
+import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
-export const POST = NextAuth({
+const authOptions = {
   adapter: MongoDBAdapter(clientPromise),
   providers: [
     GoogleProvider({
@@ -12,16 +12,7 @@ export const POST = NextAuth({
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
-});
+};
 
-export const GET = NextAuth({
-  adapter: MongoDBAdapter(clientPromise),
-
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    }),
-  ],
-  secret: process.env.NEXTAUTH_SECRET,
-});
+export const POST = NextAuth(authOptions);
+export const GET = NextAuth(authOptions);
